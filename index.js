4ce91fba2bcb85983d1c822fa55cfbeefa70bb07
Emmanuel Erim <bryonerim@gmail.com>
2022-12-13 14:39:09 +0100

    DB Added

--- index.js
+++ index.js
@@ -22,6 +22,46 @@ const responseObj={
 }
 
 
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+})
+
+const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res)=>{
    
 res.send({...responseObj, message:"Welcome", success:true })
